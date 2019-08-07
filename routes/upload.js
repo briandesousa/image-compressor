68949eed8949d9fcbe2750ed190b5ed3020a461a
Brian De Sousa <briandesousa1@gmail.com>
2019-08-07 01:35:23 -0400

    implement file upload with multer

--- routes/upload.js
+++ routes/upload.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const multer = require('multer')
+const debug = require('debug')('image-compressor:upload');
 
-router.post('/', function(req, res, next) {
-  // receive image to be uploaded
-  res.send('respond with a GUID for the uploaded image');
+const diskStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, global.appUploadDir)
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}`)
+  }
+});
+
+const upload = multer({ storage: diskStorage })
+
+function saveToSession(files) {
+  debug(`Saving info for ${files.length} uploaded file(s) to in-memory session`);
+}
+
+router.post('/', upload.array('images'), function(req, res, next) {
+  const fileUploadCount = req.files ? req.files.length : 0;
+  debug(`Uploaded file count: ${fileUploadCount}`);
+
+  if (fileUploadCount > 0) {
+    saveToSession(req.files);
+  }
+  
+  res.redirect('/');
 });
 
 /* GET users listing. */
